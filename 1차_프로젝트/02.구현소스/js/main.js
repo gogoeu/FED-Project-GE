0f77b2110ef0805c59a778c2e4dc9739579d319e
gogoeu <ko1331@naver.com>
2024-12-05 09:06:56 +0900

    3

--- 1차_프로젝트/02.구현소스/js/main.js
+++ 1차_프로젝트/02.구현소스/js/main.js
@@ -1,4 +1,11 @@
 // 메인 페이지 JS - main.js
+
+
+// 나의 함수 불러오기
+import myFn from "./my_function.js";
+import main1 from "./main1.js";
+
+
 let nav = document.querySelector("nav");
 let a = document.querySelector("header a");
 let hdd = document.querySelector(".hdd");
@@ -12,3 +19,32 @@ a.addEventListener("click", function () {
   hddul.classList.toggle("active");
 });
 console.log(window.scrollTop);
+
+
+
+
+
+
+
+
+
+const slide = myFn.qs('.slide');
+// 순번변수
+
+let seqNum = 0;
+myFn.qs('.ab2').onclick = ()=>{
+    seqNum++;
+    if(seqNum>4)seqNum = 0;
+    // 슬라이드 CSS변경하기
+    num()
+};
+myFn.qs('.ab1').onclick = ()=>{
+    seqNum--;
+    num()
+    if(seqNum<0)seqNum = 5;
+}
+
+function num(){
+    slide.style.translate = (seqNum*-100)+'%';
+    slide.style.transition = '.4s ease-in-out';
+}
